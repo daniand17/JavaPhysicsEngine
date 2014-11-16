5cd65ab2730b569ff7aa7fb27aa2e78fa84e24d4
Andrew <Andrew@Andrew-OfficePC.eau.wi.charter.com>
2014-11-16 01:56:21 -0600

    Finished the debug log scheme. Note that you can use the public constructor that takes no args and it will create a debug log of the form DBoutN.txt where "N" is the number of log created.
    Otherwise, you can specify a filename if you want to use specific names
    to categorize error output. You can also just use the Debug.Log() method
    to print things to the console. This functions the same as
    System.out.println().
    
    In the case of instantiating objects to write to files...you need to
    close the log once you're done with close(). Otherwise nothing will
    print to the file.

--- game_engine/src/game_engine/Debug.java
+++ game_engine/src/game_engine/Debug.java
@@ -1,22 +1,84 @@
 package game_engine;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Debug {
 
+	private File debugOutput;
+	private PrintWriter out;
+
+	// A constant to call the generic files
+	private final String genFilename = "DBout";
+	private String logName;
+
+	private static int debugLogCount;
+
 	/**
-	 * Constructs a Debug object to send debug messages to a file specified by the filename.
+	 * Constructs a Debug object to send debug messages to a file specified by
+	 * the filename.
 	 * 
 	 * @param filename
 	 */
 	public Debug(String filename) {
+		logName = filename + ".txt";
+		debugOutput = new File(logName);
 
+		try {
+			out = new PrintWriter(debugOutput);
+		} catch (FileNotFoundException e) {
+			System.err.println("Could not access the debug output file! Debug "
+					+ "messages not printing to file " + logName);
+		}
 	}
 
-	public static void Log(String msg) {
-		System.out.println(msg);
+	public Debug() {
+		// Creates a generic debug output file
+		logName = genFilename + debugLogCount + ".txt";
+		debugOutput = new File(logName);
+
+		// Increments the number of debug logs currently open.
+		debugLogCount++;
+
+		try {
+			out = new PrintWriter(debugOutput);
+		} catch (FileNotFoundException e) {
+			System.err.println("Could not access the debug output file! Debug "
+					+ "messages not printing to file " + logName);
+		}
 	}
 
-	public void LogF() {
-
+	/**
+	 * Simply prints the message to stdout. Calls System.out.println()
+	 * 
+	 * @param msg
+	 *            the message to print out.
+	 */
+	public static void Log(String message) {
+		System.out.println(message);
 	}
 
+	/**
+	 * Prints the message to the file specified in the constructor. If no file
+	 * was specified in the constructor, then it will be some generic debug
+	 * output file name.
+	 */
+	public void LogF(String message) {
+
+		if ( out != null && message != null )
+			out.write(message + "\n");
+		else
+			System.err
+					.println("Debug.LogF() not successful. Could not print to Debug Log: "
+							+ logName);
+	}
+
+	/**
+	 * Closes the printwriter and flushes the buffer. Call this method if you
+	 * want your debug information saved to the file.
+	 */
+	public void closeLog() {
+		out.close();
+	}
 }
--- game_engine/src/game_engine/Main.java
+++ game_engine/src/game_engine/Main.java
@@ -12,26 +12,43 @@ public class Main {
 		// Use of a static method in an abstract class
 		float dist = Vector.distance(unInitVec, initVec);
 
-		System.out.println("Distance: " + dist);
+		// Opens a new debug log with a filename specified in the method--this
+		// is useful if you want to route specific error messages to specific
+		// files.
+		Debug log1 = new Debug();
+		Debug log2 = new Debug();
+		Debug log3 = new Debug("out");
+
+		log1.LogF("Distance: " + dist);
 
 		// Use of a static method in an abstract class
 		float mag = Vector.magnitude(initVec);
-		System.out.println("Magnitude before scale: " + mag);
+		log2.LogF("Magnitude before scale: " + mag);
 
-		// Use of a method in an inheriting class that was promised to use by the abstract class
-		// that requires it. Each scale method must be implemented in the Vector2D, Vector3D, etc
-		// class. Interfaces follow a similar scheme in that they make a "contract" with the
-		// implementing class.
+		// Use of a method in an inheriting class that was promised to use by
+		// the abstract class that requires it. Each scale method must be
+		// implemented in the Vector2D, Vector3D, etc class. Interfaces follow a
+		// similar scheme in that they make a "contract" with the implementing
+		// class.
 		initVec.scale(5f);
 
 		mag = Vector.magnitude(initVec);
 		// It works!
-		System.out.println("Magnitude before scale: " + mag);
+		log1.LogF("Magnitude before scale: " + mag);
 
-		// Since the variables (aka fields) are public, we can access them directly
+		// Close the debug log once you're done with it
+		log1.closeLog();
+		log2.closeLog();
+
+		// Since the variables (aka fields) are public, we can access them
+		// directly
 		float meh = initVec.x + initVec.y;
 
-		// Note: don't make things public if you don't want people to be doing this.
+		log3.LogF("" + meh);
+		log3.closeLog();
+
+		// Note: don't make things public if you don't want people to be doing
+		// this.
 
 	}
 
