dcc17da4f4998ac84e44f7010724756ca5a4d0fc
Andrew <Andrew@Andrew-OfficePC.eau.wi.charter.com>
2014-11-16 01:57:43 -0600

    Renamed some functions.

--- game_engine/src/game_engine/Main.java
+++ game_engine/src/game_engine/Main.java
@@ -12,11 +12,10 @@ public class Main {
 		// Use of a static method in an abstract class
 		float dist = Vector.distance(unInitVec, initVec);
 
-		// Opens a new debug log with a filename specified in the method--this
-		// is useful if you want to route specific error messages to specific
-		// files.
+		// Generic debug logs
 		Debug log1 = new Debug();
 		Debug log2 = new Debug();
+		// A debug log specified with a file name.
 		Debug log3 = new Debug("out");
 
 		log1.LogF("Distance: " + dist);
@@ -37,19 +36,14 @@ public class Main {
 		log1.LogF("Magnitude before scale: " + mag);
 
 		// Close the debug log once you're done with it
-		log1.closeLog();
-		log2.closeLog();
+		log1.close();
+		log2.close();
 
 		// Since the variables (aka fields) are public, we can access them
 		// directly
 		float meh = initVec.x + initVec.y;
 
 		log3.LogF("" + meh);
-		log3.closeLog();
-
-		// Note: don't make things public if you don't want people to be doing
-		// this.
-
+		log3.close();
 	}
-
 }
