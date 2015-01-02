d3ece8fe63d401117d0b800516d1d8549c7ec521
andrew <andrew@faraday.eau.wi.charter.com>
2015-01-01 23:42:54 -0600

    Forgot to add BoxCollider2D and CircleCollider2D to the index...

---
+++ game_engine/src/game_engine/CircleCollider2D.java
@@ -0,0 +1,13 @@
+package game_engine;
+
+import java.awt.Shape;
+
+public class CircleCollider2D extends Collider {
+
+	@Override
+	Shape getBoundedArea() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
